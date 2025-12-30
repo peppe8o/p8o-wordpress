790114b8f2139fd60aeaff9d32c83dc7fd9744a4
peppe8o <68339686+peppe8o@users.noreply.github.com>
2025-12-30 18:21:27 +0100

    Delete p8o-optimize directory

--- p8o-optimize/includes/frontend/frontend.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit;
-
-require_once __DIR__ . '/css.php';
-require_once __DIR__ . '/js.php';
-require_once __DIR__ . '/images.php';
-require_once __DIR__ . '/external-js-hints.php';
-require_once __DIR__ . '/ads-cls.php';
